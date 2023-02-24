70a4799645399deeb87cd18788e0733d1078cbbf
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-24 15:35:28 +0300

    Main.java edited

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,16 @@ public class Main {
         int depositAmount = 1100; // Переменная для хранения суммы к зачислению
         boolean bonusProvided = depositAmount > 1000; // Проверка выполнения условия для начисления бонусных рублей
         int bonus; // Переменная для хранения бонусов к начислению
+        int totalSum; // Переменная для хранения итоговой суммы
 
         if (bonusProvided) {
             bonus = depositAmount / 100 * 1;
-            int totalSumWithBonus = initialAmount + depositAmount + bonus;
-            System.out.println("Итоговый счет: " + totalSumWithBonus);
+            totalSum = depositAmount + initialAmount + bonus;
+            System.out.println("Итоговый счет: " + totalSum);
             System.out.println("Бонусов начислено: " + bonus);
         } else {
             bonus = 0;
-            int totalSum = initialAmount + depositAmount;
+            totalSum = depositAmount + initialAmount + bonus;
             System.out.println("Итоговый счет: " + totalSum);
             System.out.println("Бонусов начислено: " + bonus);
         }
