483960d3295827700e237f21fcf448df28d71d7a
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-24 15:52:44 +0300

    Main.java updated + Ternary conditional operator

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,33 @@ public class Main {
         int initialAmount = 300; // Переменная для хранения изначальной суммы на счету
         int depositAmount = 1100; // Переменная для хранения суммы к зачислению
         boolean bonusProvided = depositAmount > 1000; // Проверка выполнения условия для начисления бонусных рублей
-        int bonus; // Переменная для хранения бонусов к начислению
-        int totalSum; // Переменная для хранения итоговой суммы
 
-        if (bonusProvided) {
-            bonus = depositAmount / 100 * 1;
-            totalSum = depositAmount + initialAmount + bonus;
-            System.out.println("Итоговый счет: " + totalSum);
-            System.out.println("Бонусов начислено: " + bonus);
-        } else {
-            bonus = 0;
-            totalSum = depositAmount + initialAmount + bonus;
-            System.out.println("Итоговый счет: " + totalSum);
-            System.out.println("Бонусов начислено: " + bonus);
+//     Вариант решения с использованием тернарного условного оператора:
+
+        int bonus = bonusProvided ? depositAmount / 100 * 1 : 0;
+        int totalSum = initialAmount + depositAmount + bonus;
+        System.out.println("Итоговый счет: " + totalSum);
+        System.out.println("Бонусов начислено: " + bonus);
+
+//     Вариант решения с использованием операторов if/else:
+
+//        int bonus; // Переменная для хранения бонусов к начислению
+//        int totalSum; // Переменная для хранения итоговой суммы
+
+//        if (bonusProvided) {
+//            bonus = depositAmount / 100 * 1;
+//            totalSum = depositAmount + initialAmount + bonus;
+//            System.out.println("Итоговый счет: " + totalSum);
+//            System.out.println("Бонусов начислено: " + bonus);
+//        } else {
+//            bonus = 0;
+//            totalSum = depositAmount + initialAmount + bonus;
+//            System.out.println("Итоговый счет: " + totalSum);
+//            System.out.println("Бонусов начислено: " + bonus);
+
+
+
+
         }
 
     }
-}
